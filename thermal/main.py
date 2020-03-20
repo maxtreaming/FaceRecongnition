635b884ca3e5cac92011aab1952b74e40dbeb59b
Maxime Guidon <mguidon@spyro-soft.com>
2020-03-20 11:51:10 +0100

    Send temp to postgres db

--- thermal/main.py
+++ thermal/main.py
@@ -3,22 +3,51 @@ import psycopg2
 
 data_list = []
 
-while(True):
-
-    temp = 0.0
-    date_time = time.localtime()
+def getTemp():
     with open("/sys/class/thermal/thermal_zone0/temp", 'r') as f:
         temp_str = f.readline()
         temp = float(temp_str) / 1000
-    date_time_str = time.strftime("%Y-%m-%d %H:%M:%S", date_time)
-    print("{} {}".format(date_time_str, temp))
+        return temp
 
-    data_list.append((date_time_str, temp))
+def getTime():
+    date_time = time.localtime()
+    return time.strftime("%Y-%m-%d %H:%M:%S", date_time)
 
-    if(len(data_list) == 10):
-        print("Sent")
+def sendToDb(list_of_records):
+    try:
+       connection = psycopg2.connect(user="postgres",
+                                      password="1234",
+                                      host="192.168.0.101",
+                                      port="5432",
+                                      database="postgres")
+       cursor = connection.cursor()
 
-        data_list = []
+       postgres_insert_query = """ INSERT INTO raspberrypitemperature (time, value) VALUES (%s,%s)"""
+
+       for i, element in enumerate(list_of_records):
+            cursor.execute(postgres_insert_query, element)
+
+       connection.commit()
+       count = cursor.rowcount
+       print (count, "Record inserted successfully into mobile table")
+
+    except (Exception, psycopg2.Error) as error :
+        if(connection):
+            print("Failed to insert record into mobile table", error)
+
+    finally:
+        #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
 
 
-    time.sleep(1)
+
+while 1:
+    data_to_send = []
+    for i in range(10):
+        data_to_send.append((getTime(), getTemp()))
+        time.sleep(1)
+
+    sendToDb(data_to_send)
